0839a50daca62055d918e80cbe0e52c6a49943e7
Vadym Vatsenko <69381493+vadimvatsenko@users.noreply.github.com>
2025-03-12 15:16:24 +0200

    add Legacy Input

---
+++ Assets/Scripts/Hero/Hero.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class Hero : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    private float _direction;
+    
+    public void SetDirection(float dir)
+    {
+        _direction = dir;
+    }
+
+    public void Update()
+    {
+        if (_direction != 0)
+        {
+            
+            // Time.deltaTime - возвращает нам время последнего кадра, 
+            // Time.deltaTime добавлен в расчет delta для расчёта плавного передвижения игрока,
+            // Фрейм может быть не ровным, в зависимости от загрузки процессора рассчитывается время кадра,
+            // время кадра может быть всегда разным
+            
+            float delta = _direction * speed * Time.deltaTime; // смещение объекта - delta
+            float newXpos = transform.position.x + delta;
+            transform.position = new Vector3(newXpos, transform.position.y, transform.position.z);
+        }
+    }
+
+    public void Fire()
+    {
+        Debug.Log("Fire");
+    }
+}
---
+++ Assets/Scripts/Hero/HeroInputReader.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+
+public class HeroInputReader : MonoBehaviour
+{
+    private Hero _hero;
+    //[SerializeField] private Hero hero; // если мы захотим связать через редактор
+
+    private void Awake()
+    {
+        _hero = GetComponent<Hero>();
+    }
+    private void Update()
+    {
+        //SetHeroDirection_V1();
+        SetHeroDirection_V2();
+        
+        // состояние кнопок
+        // Input.GetKey(KeyCode.Mouse0);  // GetKey - зажал
+        // Input.GetKeyDown(KeyCode.Mouse0); // GetKeyDown - нажал
+        // Input.GetKeyUp(KeyCode.Mouse0); // GetKeyUp - отпустил
+        
+        // Fire1 - это ссылка на кнопку в Project Settings => Input Manager в редакторе
+        // Input.GetButtonDown("Fire1") // GetButtonDown - для виртуальных кнопок в Project Settings => Input Manager
+
+        HeroFire();
+    }
+
+    private void HeroFire()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            _hero.Fire();
+        }
+    }
+
+    private void SetHeroDirection_V2()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        _hero.SetDirection(horizontal);
+    }
+    
+    private void SetHeroDirection_V1()
+    {
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            _hero.SetDirection(-1);
+        }
+        else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            _hero.SetDirection(1);
+        }
+        else
+        {
+            _hero.SetDirection(0);
+        }
+    }
+}
