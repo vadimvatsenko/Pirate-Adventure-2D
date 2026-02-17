c51fa0bf2bfe7f84aacd69db2893ee10d6ba4021
Vadym <69381493+vadimvatsenko@users.noreply.github.com>
2026-02-17 14:47:16 +0200

    rebiuld animation

---
+++ Assets/Scripts/Creatures/CreaturesStateMachine/CreatureBasic/Movement.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        transform.position += Vector3.right * (2 *  Time.deltaTime);
+    }
+}
