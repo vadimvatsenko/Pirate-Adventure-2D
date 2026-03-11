4c444bc9ea5bbab10b33eb68197d4fa5f8be50e6
Vadym <69381493+vadimvatsenko@users.noreply.github.com>
2026-03-11 22:44:17 +0200

    save scene

---
+++ Assets/Scripts/UI/Widgets/CustomButton.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI.Widgets
+{
+    public class CustomButton : Button
+    {
+        [SerializeField] private GameObject normal;
+        [SerializeField] private GameObject pressed;
+
+        protected override void DoStateTransition(SelectionState state, bool instant)
+        {
+            base.DoStateTransition(state, instant); 
+            
+            normal.SetActive(state != SelectionState.Pressed);
+            pressed.SetActive(state == SelectionState.Pressed);
+        }
+    }
+}
---
+++ Assets/Scripts/UI/Widgets/Editor/CustomButtonEditor.cs
@@ -0,0 +1,20 @@
+﻿using UnityEditor;
+using UnityEditor.UI;
+
+namespace UI.Widgets.Editor
+{
+    [CustomEditor(typeof(CustomButton), true)]
+    [CanEditMultipleObjects]
+    public class CustomButtonEditor : ButtonEditor
+    {
+        public override void OnInspectorGUI()
+        {
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("normal"));
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("pressed"));
+            
+            serializedObject.ApplyModifiedProperties();
+            
+            base.OnInspectorGUI();
+        }
+    }
+}
